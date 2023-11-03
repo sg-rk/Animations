bcc65470116e6c50052ec760f270a687a65a57dd
Ranjithkumar <nseit_rs@vendor.nse.co.in>
2023-11-03 18:05:52 +0530

    web worker created

---
+++ worker.js
@@ -0,0 +1,9 @@
+onmessage = (e) => {
+    console.log('message received', e.data);
+    let sum = 0;
+    for (let i = 0; i < 10000000000; i++) {
+        sum += i;
+    }
+
+    postMessage(sum);
+}
